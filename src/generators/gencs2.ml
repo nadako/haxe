69e278ea8bb01033006f6ba49111442545d33d40
Dan Korostelev <nadako@gmail.com>
2017-03-14 01:05:30 +0300

    wip

--- src/generators/gencs2.ml
+++ src/generators/gencs2.ml
@@ -1,7 +1,28 @@
 open Common
+open Type
+
+let write_file ctx pack name contents =
+	mkdir_recursive ctx.file pack;
+	let out_file = String.concat "/" (ctx.file :: pack @ [name ^ ".cs"]) in
+	let out_chan = open_out_bin out_file in
+	output_string out_chan contents;
+	close_out out_chan
+
+let generate_class ctx cl =
+	let pack,name = cl.cl_path in
+	let contents = "// TODO" in
+	write_file ctx pack name contents
+
+let generate_type ctx t =
+	match t with
+	| TClassDecl cl when not cl.cl_extern ->
+		generate_class ctx cl
+	|_ ->
+		() (* TODO *)
 
 (**
 	Entry point to Gencs2
 *)
-let generate (com:context) =
+let generate com =
+	List.iter (generate_type com) com.types;
 	print_endline "cs2"
