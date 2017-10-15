2e0ca99e54a2808dcdd6588d2d5d18e39c947580
Dan Korostelev <nadako@gmail.com>
2017-10-15 14:16:30 +0300

    unwrapping

--- src/filters/jsTryCatch.ml
+++ src/filters/jsTryCatch.ml
@@ -46,16 +46,30 @@ let transform com e =
 			| _ ->
 				let ewrap = { eerr with eexpr = TNew (clerr,[],[eerr]); etype = TInst (clerr,[]) } in
 				{ e with eexpr = TThrow ewrap })
+
 		| TCall ({ eexpr = TIdent "__rethrow__" },_) ->
 			(match rethrow_var with
 			| Some e -> { e with eexpr = TThrow e }
 			| None -> abort "rethrow outside of catch" e.epos)
+
 		| TTry (etry,catches) ->
 			let etry = loop rethrow_var etry in
 
 			let vcatchall = alloc_var "e" t_dynamic e.epos in
 			let ecatchall = make_local vcatchall e.epos in
+
+			let vunwrapped = alloc_var "e" t_dynamic e.epos in
+			let eunwrap =
+				let einstanceof = mk (TIdent "__instanceof__") t_dynamic e.epos in
+				let eterr = make_static_this clerr e.epos in
+				let echeck = mk (TCall (einstanceof,[ecatchall;eterr])) com.basic.tbool e.epos in
+				let efield = field { ecatchall with etype = TInst (clerr,[]) } "val" t_dynamic e.epos in
+				mk (TIf (echeck, efield, Some ecatchall)) t_dynamic e.epos
+			in
+			let eunwrapped = make_local vunwrapped e.epos in
+
 			let erethrow = mk (TThrow ecatchall) t_dynamic e.epos in
+
 			let ebody = List.fold_left (fun eprev (v,e) ->
 				let e = loop (Some ecatchall) e in
 
@@ -64,16 +78,25 @@ let transform com e =
 				let mt = try Some (module_type_of_type v.v_type) with Exit -> None in
 				match mt with
 				| None ->
-					e
+					mk (TBlock [
+						mk (TVar (v, Some eunwrapped)) com.basic.tvoid v.v_pos;
+						e
+					]) e.etype e.epos
 				| Some mt ->
 					let etype = make_typeexpr mt v.v_pos in
-					let echeck = fcall eboot "__instanceof" [ecatchall; etype] com.basic.tbool v.v_pos in
+					let echeck = fcall eboot "__instanceof" [eunwrapped; etype] com.basic.tbool v.v_pos in
 					let ecatchbody = mk (TBlock [
-						mk (TVar (v, Some ecatchall)) com.basic.tvoid v.v_pos;
+						mk (TVar (v, Some eunwrapped)) com.basic.tvoid v.v_pos;
 						e
 					]) e.etype e.epos in
 					mk (TIf (echeck,ecatchbody,Some eprev)) e.etype e.epos
 			) erethrow (List.rev catches) in
+
+			let ebody = mk (TBlock [
+				mk (TVar (vunwrapped, Some eunwrap)) com.basic.tvoid e.epos;
+				ebody;
+			]) e.etype e.epos in
+
 			{ e with eexpr = TTry (etry, [(vcatchall,ebody)]) }
 		| _ ->
 			Type.map_expr (loop rethrow_var) e
